340e3f3cca701e3bc651200d440be55b2e6a2090
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-05 23:36:45 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloService } from './service/hello.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  // Inject the Service Under Test
+  providers: [HelloService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/service/hello.service.ts
+++ src/app/service/hello.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HelloService {
-
-  constructor() { }
+  public getHelloWorld() {
+    return 'Hello world !';
+  }
 }
